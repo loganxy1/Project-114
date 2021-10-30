692b090620c276ea98bf3dab852025a217c99e14
loganxy1 <80921776+loganxy1@users.noreply.github.com>
2021-10-30 16:29:38 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(450, 450);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function save(){
+    save("daBaby.png");
+}
